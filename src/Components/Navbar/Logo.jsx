4dc0dc96955ec4140319aa36102001e48d367872
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-18 02:47:51 +0600

    footer design added

--- src/Components/Navbar/Logo.jsx
+++ src/Components/Navbar/Logo.jsx
@@ -1,7 +1,8 @@
 const Logo = () => {
     return (
-        <div className="ml-[160px] md:ml-0">
-            <img className="w-[100px] h-[80px]" src="https://i.ibb.co/J7Z91p0/logo.jpg" alt="" />
+        <div className="ml-[160px] md:ml-0 flex justify-center items-center">
+            <img className="w-[70px] h-[70px]" src="https://i.ibb.co/J7Z91p0/logo.jpg" alt="" />
+            <h1 className="italic font-extrabold text-[#c97d4a] overline ">SilkSerenadeStyle</h1>
         </div>
     );
 };
