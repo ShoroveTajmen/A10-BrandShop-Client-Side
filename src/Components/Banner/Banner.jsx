d537fec383840d6ab730d743797af934b38d8e03
ShoroveTajmen <sorovitajmen@gmail.com>
2023-10-18 04:14:58 +0600

    brand section created and display all brand dynamically

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="bg-[#DCDCDC] w-full h-[500px] border border-red-600">
+    <div className="bg-[#DCDCDC] w-full h-[500px]">
       <div className="flex justify-center items-center w-[1300px] gap-[40px] mx-auto">
         <div>
           <img className="mt-[100px] shadow-2xl rounded-full" src="https://i.ibb.co/CV0w1Sc/shopping.jpg" alt="" />
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../Components/Banner/Banner";
+import AllData from "../../Components/AllData/AllData";
 
 const Home = () => {
-    const data = useLoaderData();
-    console.log(data);
-    return (
-        <div>
-            <Banner></Banner>
-        </div>
-    );
+  const data = useLoaderData();
+  console.log(data);
+  return (
+    <div>
+      <Banner></Banner>
+      <div className="mt-[70px]">
+        {" "}
+        <h1 className="text-center font-bold text-4xl">Explore Your Favourite Brands</h1>
+      </div>
+
+      <AllData data={data}></AllData>
+    </div>
+  );
 };
 
 export default Home;
